73017f22ab4e9c665fdced86060acf25ee0003bc
ivanrlmx99 <ivanlaureano77@gmail.com>
2020-06-14 15:25:46 -0500

    h2 proyecto crud

---
+++ src/main/resources/import.sql
@@ -0,0 +1,14 @@
+/* Populate tables */
+INSERT INTO clientes (id, nombre, apellido, email, create_at) VALUES(1, 'Andres', 'Guzman', 'profesor@bolsadeideas.com', '2017-08-28');
+INSERT INTO clientes (id, nombre, apellido, email, create_at) VALUES(2, 'John', 'Doe', 'john.doe@gmail.com', '2017-08-28');
+INSERT INTO clientes (id, nombre, apellido, email, create_at) VALUES(3, 'Ivan', 'Ramos', 'ivanmx@gmail.com', '2020-06-14');
+INSERT INTO clientes (id, nombre, apellido, email, create_at) VALUES(4, 'karola', 'ballenato', 'karr@gmail.com', '2019-08-28');
+INSERT INTO clientes (id, nombre, apellido, email, create_at) VALUES(5, 'Endejelin', 'lopez', 'endejelo@gmail.com', '2017-08-28');
+INSERT INTO clientes (id, nombre, apellido, email, create_at) VALUES(6, 'perla', 'laureano', 'perla@gmail.com', '2017-08-28');
+INSERT INTO clientes (id, nombre, apellido, email, create_at) VALUES(7, 'citlali', 'gomez', 'citla@gmail.com', '2017-08-28');
+INSERT INTO clientes (id, nombre, apellido, email, create_at) VALUES(8, 'kendra', 'guzman', 'kendra@gmail.com', '2017-08-28');
+INSERT INTO clientes (id, nombre, apellido, email, create_at) VALUES(9, 'mirella', 'sanchez', 'mire@gmail.com', '2017-08-28');
+INSERT INTO clientes (id, nombre, apellido, email, create_at) VALUES(10, 'itzelth', 'gonzalez', 'itza@gmail.com', '2017-08-28');
+INSERT INTO clientes (id, nombre, apellido, email, create_at) VALUES(11, 'aurora', 'boreal', 'aury@gmail.com', '2017-08-28');
+INSERT INTO clientes (id, nombre, apellido, email, create_at) VALUES(12, 'liseth', 'martinez', 'lisy@gmail.com', '2017-08-28');
+INSERT INTO clientes (id, nombre, apellido, email, create_at) VALUES(13, 'fernanda', 'del rio', 'ferny@gmail.com', '2017-08-28');
